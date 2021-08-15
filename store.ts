0a6fc8abbf7da6c98600967fd59e11a2b7d58f53
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2021-08-15 22:10:56 +0200

    Add local data persistence

--- store.ts
+++ store.ts
@@ -1,14 +1,47 @@
-import { Action, configureStore, ThunkAction } from '@reduxjs/toolkit';
+import { Action, combineReducers, configureStore, getDefaultMiddleware, ThunkAction } from '@reduxjs/toolkit';
+import {
+  persistReducer,
+  persistStore,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'reduxjs-toolkit-persist';
+import storage from 'reduxjs-toolkit-persist/lib/storage';
+import autoMergeLevel1 from 'reduxjs-toolkit-persist/lib/stateReconciler/autoMergeLevel1';
+
 import recipeRatingSlice from './src/redux/recipeRatingSlice';
 import searchCacheSlice from './src/redux/searchCacheSlice';
 
-export const store = configureStore({
-  reducer: {
-    searchCache: searchCacheSlice,
-    recipeRating: recipeRatingSlice,
-  },
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  stateReconciler: autoMergeLevel1,
+  whitelist: ['recipeRating'],
+};
+
+const reducers = combineReducers({
+  searchCache: searchCacheSlice,
+  recipeRating: recipeRatingSlice,
 });
+
+const _persistedReducer = persistReducer<any, any>(persistConfig, reducers);
+
+const store = configureStore({
+  reducer: _persistedReducer,
+  middleware: getDefaultMiddleware({
+    serializableCheck: {
+      /* ignore persistance actions */
+      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    },
+  }),
+});
+
+const persistor = persistStore(store);
 
 export type AppDispatch = typeof store.dispatch;
 export type RootState = ReturnType<typeof store.getState>;
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootState, unknown, Action<string>>;
+export { store, persistor };
