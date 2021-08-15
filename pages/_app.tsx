44c57cc3381d4bd95d6232deb8c13cbc6e9747e7
Aleksander Błaszkiewicz <54682207+ablaszkiewicz@users.noreply.github.com>
2021-08-15 15:23:23 +0200

    Add redux-toolkit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,18 +4,22 @@ import { ChakraProvider } from '@chakra-ui/react';
 import { AnimatePresence, motion } from 'framer-motion';
 import React, { useEffect } from 'react';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
+import { store } from '../store';
 
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
     <ChakraProvider>
-      <Head>
-        <link rel='preconnect' href='https://fonts.googleapis.com' />
-        <link rel='preconnect' href='https://fonts.gstatic.com' />
-        <link href='https://fonts.googleapis.com/css2?family=Lato:wght@900&display=swap' rel='stylesheet' />
-      </Head>
-      <AnimatePresence exitBeforeEnter>
-        <Component {...pageProps} key={router.route} />
-      </AnimatePresence>
+      <Provider store={store}>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link rel='preconnect' href='https://fonts.gstatic.com' />
+          <link href='https://fonts.googleapis.com/css2?family=Lato:wght@900&display=swap' rel='stylesheet' />
+        </Head>
+        <AnimatePresence exitBeforeEnter>
+          <Component {...pageProps} key={router.route} />
+        </AnimatePresence>
+      </Provider>
     </ChakraProvider>
   );
 }
